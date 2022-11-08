aef4b3f65363ab40ad65572d858adc01806d6e6c
Reece1109 <reecesimon84@gmail.com>
2022-11-08 13:34:50 +0200

    main

---
+++ js/code.js
@@ -0,0 +1,42 @@
+//Addtion 
+function addition(){
+console.log(5 + 2);
+}
+
+//Subtraction
+function subtracting(){
+    console.log(5 - 2);
+}
+
+//Multiplication
+function multiplication(){
+    console.log(5 * 2);
+}
+
+//Division
+function division(){
+    console.log(5/2);
+}
+
+//Exponent
+function exponent(){
+    console.log(2**2);
+}
+
+//Modulus
+functionmodulus(){
+    console.log(5%2)
+}
+
+Call all functions
+// addition();
+// subtracting();
+// multiplication();
+// division();
+// exponent();
+// modulus();
+let number1 =4;
+let number2 = 6;
+let result = 0
+result+= number1 + number2;
+console.log(result);
