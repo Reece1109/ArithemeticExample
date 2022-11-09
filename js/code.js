36996a43e53ab63067e42d3c3f09f3779d2b0dc7
Reece1109 <reecesimon84@gmail.com>
2022-11-09 14:20:02 +0200

    main

--- js/code.js
+++ js/code.js
@@ -1,42 +1,74 @@
-//Addtion 
-function addition(){
-console.log(5 + 2);
-}
+// //Addtion 
+// function addition(){
+// console.log(5 + 2);
+// }
 
-//Subtraction
-function subtracting(){
-    console.log(5 - 2);
-}
+// //Subtraction
+// function subtracting(){
+//     console.log(5 - 2);
+// }
 
-//Multiplication
-function multiplication(){
-    console.log(5 * 2);
-}
+// //Multiplication
+// function multiplication(){
+//     console.log(5 * 2);
+// }
 
-//Division
-function division(){
-    console.log(5/2);
-}
+// //Division
+// function division(){
+//     console.log(5/2);
+// }
 
-//Exponent
-function exponent(){
-    console.log(2**2);
-}
+// //Exponent
+// function exponent(){
+//     console.log(2**2);
+// }
 
-//Modulus
-functionmodulus(){
-    console.log(5%2)
-}
+// //Modulus
+// functionmodulus(){
+//     console.log(5%2)
+// }
 
-Call all functions
-// addition();
-// subtracting();
-// multiplication();
-// division();
-// exponent();
-// modulus();
-let number1 =4;
-let number2 = 6;
-let result = 0
-result+= number1 + number2;
-console.log(result);
+// Call all functions
+// // addition();
+// // subtracting();
+// // multiplication();
+// // division();
+// // exponent();
+// // modulus();
+// // let number1 =4;
+// // let number2 = 6;
+// // let result = 0
+// // result+= number1 + number2;
+// // console.log(result);
+// let numb1 = 3;
+// numb1+ numb1+= 2;
+// console.log(numb1)
+
+
+
+//console.log(numb1 <=numb2);
+//console.log(numb1 >=numb2);
+//console.log(numb1 !=numb2);
+//console.log(numb1 ==numb2);
+//console.log(numb1 ===numb2);
+// let salary = prompt("Enter your salary");
+// let age = prompt(parseInt(prompt("Enter your salary:")));
+// let qualified = (salary >= 5000) ||
+// (age > 17);
+
+// console.log.apply(qualified);
+
+// Relational operator in
+// let numbers = [9,7,3,15];
+// //console.log(7 in numbers);
+// //Increment and decrement
+// let numb1 = 8
+// console.log(numb1++);
+// //console.log((numb1);
+
+//Ternary operator
+// ? :
+let age = 17
+console.log(
+    age>17 ? "You are qualified" :
+"You are not qualified");
